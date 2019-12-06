aac2bd3d35c1d762a6eb0343c2b46522eedf611a
Juan Sebastian Leal <JuanSebastian.Leal@beazley.com>
2019-12-06 15:24:27 -0500

    Add facade

---
+++ Facade/TestPosts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BrokerAPIs.Abstraction;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrokerAPIs.Facade
+{
+    [TestClass]
+    public class TestPosts
+    {
+        private Posts _posts;
+
+        [TestMethod]
+        public void GetAllPosts()
+        {
+            _posts = new Posts("get");
+            _posts.PostsRequest("get");
+            _posts.PostsValidations("get");
+        }
+
+        [TestMethod]
+        public void CreateANewPost()
+        {
+            _posts = new Posts("post");
+            _posts.PostsRequest("post");
+            _posts.PostsValidations("post");
+        }
+
+
+    }
+}
